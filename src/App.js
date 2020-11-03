8a8f4e71a0bfc4199e0d104d901bb75de5a9b7c5
Thomas Shamp <T.shamp7@gmail.com>
2020-11-02 17:41:46 -0800

    Creare Logs component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
 
 const App = () => {
   useEffect(() => {
     // Initialize Materialize JS
     M.AutoInit();
   });
-  return <div className="App">My App</div>;
+  return (
+    <div className="App">
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <Logs />
+        </div>
+      </Fragment>
+    </div>
+  );
 };
 
 export default App;
